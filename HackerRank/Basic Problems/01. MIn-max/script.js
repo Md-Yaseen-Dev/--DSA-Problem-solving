8648bdb50b40456cd16832e3982b9c539d6c1f72
mohammedyaseen2211 <ysn6040@gmail.com>
2023-07-22 19:44:09 +0530

    min max completed

---
+++ HackerRank/Basic Problems/01. MIn-max/script.js
@@ -0,0 +1,89 @@
+// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+// Example
+
+// The minimum sum is  and the maximum sum is . The function prints
+
+// 16 24
+// Function Description
+
+// Complete the miniMaxSum function in the editor below.
+
+// miniMaxSum has the following parameter(s):
+
+// arr: an array of  integers
+// Print
+
+// Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+
+// Input Format
+
+// A single line of five space-separated integers.
+
+// Constraints
+
+
+// Output Format
+
+// Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+// Sample Input
+
+// 1 2 3 4 5
+// Sample Output
+
+// 10 14
+// Explanation
+
+// The numbers are , , , , and . Calculate the following sums using four of the five integers:
+
+// Sum everything except , the sum is .
+// Sum everything except , the sum is .
+// Sum everything except , the sum is .
+// Sum everything except , the sum is .
+// Sum everything except , the sum is .
+// Hints: Beware of integer overflow! Use 64-bit Integer.
+
+// Need help to get started? Try the Solve Me First problem
+
+
+
+function miniMaxSum(arr) {
+
+    arr.sort((a, b) => a - b)
+    let min = arr[0];
+
+    let max = arr[1];
+
+    for (let i = 1; i < arr.length; i++) {
+
+
+        if (i > 1) {
+
+            max += arr[i]
+
+        }
+        if (i < 4) {
+
+            min += arr[i]
+        }
+    }
+
+    return [min, max]
+}
+
+console.log(miniMaxSum([1, 2, 8, 4, 5]));
+
+
+// another method
+
+function minmaxsum(arr) {
+
+    let total = arr.reduce((a, b) => a + b)
+
+    let min = total - Math.max(...arr);
+    let max = total - Math.min(...arr);
+
+    return [min, max]
+}
+console.log(minmaxsum([1, 2, 9, 4, 5]));
